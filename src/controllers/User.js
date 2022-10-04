3f614d91b42fab454e7b1d48167f6047352eb743
Gabriel Quintão <gabrielqgoncalves26@gmail.com>
2022-10-04 20:54:56 -0300

    security fixes for users

--- src/controllers/User.js
+++ src/controllers/User.js
@@ -4,7 +4,8 @@ class UserController {
   async store(req, res) {
     try {
       const newUser = await User.create(req.body);
-      return res.json(newUser);
+      const { id, name, email } = newUser;
+      return res.json({ id, name, email });
     } catch (e) {
       return res.status(400).json({
         errors: e.errors.map((err) => err.message),
@@ -15,8 +16,6 @@ class UserController {
   async index(req, res) {
     try {
       const users = await User.findAll();
-      console.log('User ID:', req.userId);
-      console.log('User Email:', req.userEmail);
       return res.json(users);
     } catch (e) {
       return res.json(null);
@@ -34,13 +33,7 @@ class UserController {
 
   async update(req, res) {
     try {
-      if (!req.params.id) {
-        return res.status(400).json({
-          errors: ['Missing ID.'],
-        });
-      }
-
-      const user = await User.findByPk(req.params.id);
+      const user = await User.findByPk(req.userId);
 
       if (!user) {
         return res.status(400).json({
@@ -49,8 +42,9 @@ class UserController {
       }
 
       const newData = await user.update(req.body);
+      const { id, name, email } = newData;
 
-      return res.json(newData);
+      return res.json({ id, name, email });
     } catch (e) {
       return res.status(400).json({
         errors: e.errors.map((err) => err.message),
@@ -60,13 +54,7 @@ class UserController {
 
   async delete(req, res) {
     try {
-      if (!req.params.id) {
-        return res.status(400).json({
-          errors: ['Missing ID.'],
-        });
-      }
-
-      const user = await User.findByPk(req.params.id);
+      const user = await User.findByPk(req.userId);
 
       if (!user) {
         return res.status(400).json({
--- src/routes/user.js
+++ src/routes/user.js
@@ -8,11 +8,12 @@ import loginRequired from '../middlewares/loginRequired';
 
 const router = new Router();
 
+//router.get('/', loginRequired, userController.index); HAS NO USE, DID JUST TO SEE HOW IT WORKS
+//router.get('/:id', userController.show);
+
 router.post('/', userController.store); //userController.create makes the same
-router.get('/', loginRequired, userController.index);
-router.get('/:id', userController.show);
-router.put('/:id', userController.update);
-router.delete('/:id', userController.delete);
+router.put('/', loginRequired, userController.update);
+router.delete('/', loginRequired, userController.delete);
 
 export default router;
 
