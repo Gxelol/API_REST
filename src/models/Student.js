2a457880e4113a2333e1c56e41b5954182499a99
Gabriel Quintão <gabrielqgoncalves26@gmail.com>
2022-10-04 23:39:14 -0300

    students CRUD

--- src/models/Student.js
+++ src/models/Student.js
@@ -3,12 +3,59 @@ import Sequelize, { Model } from 'sequelize';
 export default class Student extends Model {
   static init(sequelize) {
     super.init({
-      name: Sequelize.STRING,
-      surname: Sequelize.STRING,
-      email: Sequelize.STRING,
-      age: Sequelize.INTEGER,
-      weight: Sequelize.FLOAT,
-      height: Sequelize.FLOAT,
+      name: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [3, 55],
+            msg: 'Name needs to have between 3 and 55 characters',
+          },
+        },
+      },
+      surname: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+      },
+      email: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        unique: {
+          msg: 'Email alrady exists',
+        },
+        validate: {
+          isEmail: {
+            msg: 'Invalid email',
+          },
+        },
+      },
+      age: {
+        type: Sequelize.INTEGER,
+        defaultValue: '',
+        validate: {
+          isInt: {
+            msg: 'Age needs to be an integer number',
+          },
+        },
+      },
+      weight: {
+        type: Sequelize.FLOAT,
+        defaultValue: '',
+        validate: {
+          isFloat: {
+            msg: 'Weight needs to be an number',
+          },
+        },
+      },
+      height: {
+        type: Sequelize.FLOAT,
+        defaultValue: '',
+        validate: {
+          isFloat: {
+            msg: 'Height needs to be an number',
+          },
+        },
+      },
     }, {
       sequelize,
     });
