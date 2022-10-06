304b47d860664cd8733e3d0571146663cd0ad4e4
Gabriel Quintão <gabrielqgoncalves26@gmail.com>
2022-10-06 20:52:00 -0300

    bad status for controller

--- src/controllers/Home.js
+++ src/controllers/Home.js
@@ -2,8 +2,14 @@ import Student from '../models/Student';
 
 class HomeController {
   async index(req, res) {
-    const newStudent = await Student.create(req.body);
-    res.json(newStudent);
+    try {
+      const newStudent = await Student.create(req.body);
+      res.json(newStudent);
+    } catch (e) {
+      res.status(400).json({
+        errors: ['Someething went wrong'],
+      });
+    }
   }
 }
 
