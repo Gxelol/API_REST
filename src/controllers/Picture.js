d02d0518f20f2379ec596fa24f3d955237e476c9
Gabriel Quintão <gabrielqgoncalves26@gmail.com>
2022-10-06 23:20:12 -0300

    treating sent files

--- src/controllers/Picture.js
+++ src/controllers/Picture.js
@@ -1,12 +1,19 @@
+import multer from 'multer';
+import multerConfig from '../config/multer';
+
+const upload = multer(multerConfig).single('picture');
+
 class PictureController {
   async store(req, res) {
-    try {
-      res.json(req.file);
-    } catch (e) {
-      res.status(400).json({
-        errors: ['Something went wrong'],
-      });
-    }
+    return upload(req, res, (error) => {
+      if (error) {
+        res.status(400).json({
+          errors: ['Something went wrong'],
+        });
+      }
+
+      return res.json(error.code);
+    });
   }
 }
 
--- src/routes/picture.js
+++ src/routes/picture.js
@@ -1,13 +1,9 @@
 import { Router } from 'express';
-import multer from 'multer';
 
 import pictureController from '../controllers/Picture';
-import multerConfig from '../config/multer';
-
-const upload = multer(multerConfig);
 
 const router = new Router();
 
-router.post('/', upload.single('picture'), pictureController.store);
+router.post('/', pictureController.store);
 
 export default router;
